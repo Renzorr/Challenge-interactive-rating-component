bd39099c4595a930c261d4d2a728f4642352773e
renzorr <benaventer01@gmail.com>
2022-12-15 21:33:04 -0500

    add sweetalert

--- js/index.js
+++ js/index.js
@@ -5,6 +5,18 @@ const cardSelected = document.querySelector(".card-selected");
 const optionSelected = document.querySelector(".option");
 let rating;
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
+
 const animation = () => {
   anime({
     targets: ".card",
@@ -14,17 +26,26 @@ const animation = () => {
   });
 };
 
+const validateOption = () => {
+  if (rating === undefined) {
+    Toast.fire({
+      icon: "error",
+      title: "Select an option",
+    });
+  } else {
+    cardSelected.classList.remove("inactive");
+    cardOption.classList.add("inactive");
+    optionSelected.innerHTML = rating;
+    animation();
+  }
+};
+
 btnRating.forEach((e) => {
   e.addEventListener("click", () => {
     rating = e.value;
   });
 });
 
-submit.addEventListener("click", () => {
-  cardSelected.classList.remove("inactive");
-  cardOption.classList.add("inactive");
-  optionSelected.innerHTML = rating;
-  animation();
-});
+submit.addEventListener("click", validateOption);
 
 animation();
